e8524e50f7e6001ad71f35577ae2733a847a0874
ielole <ieloletin@gmail.com>
2016-12-19 14:44:23 -0500

    Implement get one product.
    - Save products data in store.

--- assets/scripts/product-cart/ui.js
+++ assets/scripts/product-cart/ui.js
@@ -1,10 +1,11 @@
 'use strict';
-
+const store = require('../store.js');
 const showAllProductsTemplate = require('../templates/product-thumb.handlebars');
 
 const showOneProductTemplate = require('../templates/products.handlebars');
 
 const getAllProductsSuccess = function(data) {
+  store.products = data.products;
   console.log(data);
    $('#all-products').html(showAllProductsTemplate(data));
 
