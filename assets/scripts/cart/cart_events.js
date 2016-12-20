13bc35effe3c01caba06a3977c728aec15826bf1
rickdean <rickdean@outlook.com>
2016-12-20 09:13:08 -0500

    Add Stripe integration
    Add stripe integration using test data prior to working cart
    integration.

---
+++ assets/scripts/cart/cart_events.js
@@ -0,0 +1,50 @@
+'use strict';
+
+const cartStorage = require('./cart_storage.js');
+
+const onRemoveFromCart = function (event) {
+  event.preventDefault();
+  let targetName = $(this).parent().parent().attr('id');
+  let arr = cartStorage.cartObj.items;
+  for (let i = 0; i < arr.length; i++) {
+    let val = arr[i];
+    if (val.name === targetName) {
+      arr.splice(i, 1);
+    }
+  }
+  $(this).parent().parent().html("");
+  cartStorage.updateTotal();
+  if (arr.length < 1) {
+    $('.no-items').show();
+  }
+};
+
+const onChangeQuant = function (event) {
+  event.preventDefault();
+  let id = $(this).parent().parent().attr('id');
+  let index = cartStorage.cartObj.items
+    .findIndex(val => val.name === id);
+  let target = cartStorage.cartObj.items[index];
+  target.quantity = $(this).val();
+  cartStorage.updateTotal();
+  if ($(this).val() < 1) {
+    $(this).parent().parent().html("");
+    $('.no-items').show();
+    cartStorage.cartObj.items.splice(index, 1);
+  }
+  $('.quant').off('mouseleave');
+};
+
+const onSelectQuant = function (event) {
+  event.preventDefault();
+  $(this).on('mouseleave', onChangeQuant);
+};
+
+const addCartHandlers = () => {
+  $(document).on('click','.x', onRemoveFromCart);
+  $(document).on('click', '.quant', onSelectQuant);
+};
+
+module.exports = {
+  addCartHandlers
+};
--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,7 +1,11 @@
 'use strict';
 
 const authEvents = require('../scripts/auth/events.js');
+const cartEvents = require('../scripts/cart/cart_events.js');
+const stripeEvents = require('../scripts/stripe/stripe-events.js');
 
 $(()=>{
   authEvents.addHandlers();
+  cartEvents.addCartHandlers();
+  stripeEvents.addHandlers();
 });
