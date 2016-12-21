0e1288e58ef1a112d8b00ddc1542a0cb94f5f8ec
akadane87 <akadane87@gmail.com>
2016-12-21 06:26:20 -0500

    npm installed: scrollReveal ParallaxElement attempt at cog scroll animation images folder update changes to all handlebars templates

--- assets/index.js
+++ assets/index.js
@@ -3,11 +3,13 @@
 const authEvents = require('./auth/events.js');
 //const setAPIOrigin = require('../../lib/set-api-origin');
 const config = require('./config');
-
+const gearScroll = require('./animations.gears.js')
 //const stripeEvents = require('./stripe/stripe-events.js');
 
 $(() => {
 //  setAPIOrigin(location, config);
 //  stripeEvents.addHandlers();
   authEvents.addHandlers();
+  gearScroll.gearScroll();
+  gearScroll.browser_transform();
 });
--- index.js
+++ index.js
@@ -3,9 +3,18 @@
 // user require with a reference to bundle the file and use it in this file
 // var example = require('./example');
 
+// load parallax-element
+require('parallax-element');
+
+//load scrollreveal
+const sr = require('scrollreveal');
+
 // load manifests
 // scripts
 require('./assets/scripts/index.js');
+
+
+
 
 // styles
 require('./assets/styles/index.scss');
