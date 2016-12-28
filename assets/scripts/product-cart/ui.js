0700af6c30f088be1a00be57325518553134dc30
akadane87 <akadane87@gmail.com>
2016-12-28 11:26:50 -0500

    adding ariel's product-ui so avoid merge conflicts this removes scrollreveal animation call but I have snippet locally

--- assets/scripts/product-cart/ui.js
+++ assets/scripts/product-cart/ui.js
@@ -4,8 +4,7 @@ const showAllProductsTemplate = require('../templates/product-thumb.handlebars')
 
 const showOneProductTemplate = require('../templates/products.handlebars');
 const cartTemplate = require('../templates/cart.handlebars');
-const sr = require('../animations/scroll-reveal.js');
-
+const orderHxTemplate = require('../templates/order-history.handlebars');
 
 const StripeCheckoutSuccess = function(){
   console.log('You\'ve successfully placed your order.');
@@ -15,35 +14,42 @@ const getAllProductsSuccess = function(data) {
   store.products = data.products;
   console.log('this is get all product data.products', data.products);
    $('#all-products').html(showAllProductsTemplate(data));
-   sr.revealWatch('.product-title', '.product-thumb');
+
+
 };
 
 const getAllProductsFailure = function() {
-
+  console.log('FAIL to get all Products!');
 };
 
 const getOneProductSuccess = function(data) {
   console.log('Yay data', data);
    $('#one-product').html(showOneProductTemplate(data));
+  //  $('.cart-add').hide();
    return data;
 };
 
 const getOneProductFailure = function() {
-  console.log('FAIL!');
+  console.log('FAIL to get ONE Product!');
 };
 
 const addItemSuccess = function(data) {
   console.log('Item in cart', data);
+  $('#cart-button').show();
+  $('#checkout-button').show();
   //  $('').html((data));
   //  return data;
 };
 
 const addItemFailure = function(error) {
   console.log('FAIL!', error);
+  console.log('Log in to add items to your cart.');
 };
 
 const getItemsSuccess = function(data) {
+  store.user.serialized = data.serialized;
   console.log('I\'m in the cart! This is my item data: ', data);
+  // console.log('I\'m data.items', data.serialized);
    $('.cart-items').html(cartTemplate(data));
 };
 
@@ -92,6 +98,37 @@ const getPriceTotalFailure = function(error) {
   console.log('FAIL!, this is the getPriceTotal error.', error);
 };
 
+const clearCart = function(){
+  console.log('I\'m clearing the cart');
+  store.user.serialized = [];
+  $('.cart-items').html('Your order has been placed. View your Purchases to see past orders.');
+  $('#checkout-button').hide();
+};
+
+const createOrderHxSuccess = function(data) {
+  console.log('This is your NEW ORDER hx:', data);
+  console.log('This is your data.order._id:', data.order._id);
+  console.log('This is your data.order.items:', data.order.items);
+  console.log('This is your data.order.createdAt:', data.order.createdAt);
+  clearCart();
+  console.log('This is your store.user.serialized', store.user.serialized);
+};
+
+const createOrderHxFailure = function(error) {
+  console.log('FAIL!, this is the NEW order hx error:', error);
+};
+
+const getOrderHxSuccess = function(data) {
+  console.log('This is your order hx:', data);
+  $('#order-history').html(orderHxTemplate(data));
+};
+
+const getOrderHxFailure = function(error) {
+  console.log('FAIL!, this is the order hx error:', error);
+};
+
+
+
 module.exports = {
   StripeCheckoutSuccess,
   getAllProductsSuccess,
@@ -108,5 +145,10 @@ module.exports = {
   updateItemFailure,
   getPriceTotalSuccess,
   getPriceTotalFailure,
+  createOrderHxSuccess,
+  createOrderHxFailure,
+  getOrderHxSuccess,
+  getOrderHxFailure,
+  clearCart,
 
 };
